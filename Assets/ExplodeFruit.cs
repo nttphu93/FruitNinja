0418bdf336180b140972c9ceecf5e3bcff347bf6
nttphu93 <nttphu93@gmail.com>
2019-07-18 18:45:52 +0700

    Basic explode fruit

---
+++ Assets/ExplodeFruit.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplodeFruit : MonoBehaviour
+{
+    [SerializeField] GameObject fruitFragments;
+
+    //Called by Unity
+    private void OnMouseDown()
+    {
+        StartCoroutine(ExplodingFruit());
+    }
+    IEnumerator ExplodingFruit()
+    {
+        //gameObject.SetActive(false);
+        GameObject fragments = Instantiate(fruitFragments, transform.position, transform.rotation, transform.parent);
+        yield return new WaitForSeconds(2f);
+        Destroy(fragments);
+    }
+}
